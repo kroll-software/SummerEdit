a8e00af581a2cc061c94f796dbf1643c329ef9cf
Detlef Kroll <kroll@kroll-software.ch>
2019-01-12 14:33:01 +0100

    several issues

--- MainForm.cs
+++ MainForm.cs
@@ -11,34 +11,46 @@ namespace SummerEdit
 	{
 		public TextEditorEnsemble m_Editor;
 
-		IGuiMenuItem mnuOpen;
-		IGuiMenuItem mnuNew;
+        IGuiMenuItem mnuNew;
+        IGuiMenuItem mnuOpen;		
+        IGuiMenuItem mnuSave;
 
-		public MainForm () : base("SummerGUI Demo", 800, 600)
+        public MainForm () : base("SummerGUI Demo", 800, 600)
 		{
 			this.Title = "SummerEdit - X-Platform Texteditor";
 
 			this.TabMain.AdTabPage ("texteditor", "Editor", Color.Empty, (char)FontAwesomeIcons.fa_edit);
 			m_Editor = this.TabMain.TabPages ["texteditor"].AddChild (new TextEditorEnsemble ("editor1"));
 
-			mnuOpen = this.MenuPanel.MainMenu.FindItem ("Open");
-			mnuNew = this.MenuPanel.MainMenu.FindItem ("New");
+            mnuNew = this.MenuPanel.MainMenu.FindItem("New");
+            mnuNew.Click += (object sender, EventArgs e) =>
+            {
+                m_Editor.Text = null;
+            };
 
-			/***
-			mnuOpen.Click += (object sender, EventArgs e) => 
-				StandardDialogs.OpenFileDialog("SummerEditor", 
-					Strings.ApplicationPath(true),
-					new string[]{"", ""}, "Text Files", false);	
-			***/
-
+            mnuOpen = this.MenuPanel.MainMenu.FindItem ("Open");
 			mnuOpen.Click += (object sender, EventArgs e) => {
 				SummerGUI.SystemSpecific.Linux.SystemDialogs dlg = new SummerGUI.SystemSpecific.Linux.SystemDialogs();									
 				string fpath = dlg.OpenFileDialog("Open File", this);
+                if (Strings.FileExists(fpath))
+                {
+                    ShowStatus(String.Format("Loading {0}..", Strings.GetFilename(fpath)), true);
+                    System.Threading.Tasks.Task.Factory.StartNew(() => {
+                        m_Editor.FilePath = fpath;
+                        m_Editor.Text = TextFile.LoadTextFile(fpath);
+                    }).ContinueWith(t => ShowStatus());
+                }
 			};
 
-			//this.LeftSideMenu.Style.BackColorBrush = new LinearGradientBrush (Theme.Colors.Base0, Theme.Colors.Base00);
+            mnuSave = this.MenuPanel.MainMenu.FindItem("Save");
+            mnuSave.Click += (object sender, EventArgs e) =>
+            {
+                this.LogWarning("mnuSave not implemented.");
+            };
 
-			m_Editor.Focus ();
+            //this.LeftSideMenu.Style.BackColorBrush = new LinearGradientBrush (Theme.Colors.Base0, Theme.Colors.Base00);
+
+            m_Editor.Focus ();
 		}
 
         protected override void Dispose(bool manual)
